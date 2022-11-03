c9a36a3a77748c02843cfccbd7ab50a8c3025ad1
raipen <raipen@knu.ac.kr>
2022-11-03 22:07:04 +0900

    hw5

--- lab05/hw05_sigaction.c
+++ lab05/hw05_sigaction.c
@@ -1,32 +1,42 @@
+/** 2021114335 박지원
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
 
-void timeout2(int sig){
+void parentTimeOut(int sig){
+   static int elapsed = 0;
+   elapsed += 2;
    if(sig==SIGALRM)
-      puts("parent: Timeout!");
+      printf("<Parent> time out: 2, elapsed time: %d seconds\n", elapsed);
    alarm(2);
 }
 
-void timeout5(int sig){
+void childTimeOut(int sig){
+   static int elapsed = 0;
+   elapsed += 5;
    if(sig==SIGALRM)
-      puts("child: Timeout!");
+      printf("[Child] time out: 5, elapsed time: %d seconds\n", elapsed);
    alarm(5);
 }
 
-void (*setTime(int time))(int){
-   switch(time){
-   case 2:
-      return timeout2;
-   case 5:
-      return timeout5;
-   }
-   return NULL;
+void childExit(int sig){
+   int status;
+   pid_t id = waitpid(-1, &status, WNOHANG);
+   if(WIFEXITED(status))
+      printf("Child id: %d, sent: %d\n", id, WEXITSTATUS(status));
 }
 
 int main(void){
+   struct sigaction childAct;
+   childAct.sa_handler = childExit;
+   sigemptyset(&childAct.sa_mask);
+   childAct.sa_flags = 0;
+   sigaction(SIGCHLD, &childAct, 0);
+
    pid_t pid = fork();
    struct sigaction act;
    sigemptyset(&act.sa_mask);
@@ -35,18 +45,19 @@ int main(void){
    case -1:
       exit(1);
    case 0:
-      act.sa_handler = setTime(5);
+      act.sa_handler = childTimeOut;
       sigaction(SIGALRM, &act,0);
       alarm(5);
       for(int i = 0;i<20;i++)
-         sleep(100);
+         sleep(1);
+      return 5;
       break;
    default:
-      act.sa_handler = setTime(2);
+      act.sa_handler = parentTimeOut;
       sigaction(SIGALRM, &act,0);
-      alarm(5);
+      alarm(2);
       while(1)
-         sleep(100);
+         sleep(1);
       break;
    }
    return 0;
